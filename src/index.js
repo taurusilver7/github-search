670ce1888bec497515171da9a5b678785d0adfe5
taurusilver7 <prasad20697@gmail.com>
2021-02-04 10:03:14 +0530

    index files added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+import { GithubProvider } from "./context/Context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// domain & client id are obtained by Auth0.com users.
+// The redirect url is set to localhost:3000 in the auth0 application settings page. later, change it to the url of the hosting application.
+//  cachedlocation is set to local storage b/c, the project redirects the user to login page, logging the user out after a 404 error loading page. It needs to redirect the user to the dashboard insted of logging out.
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain="taurusilver.us.auth0.com"
+    clientId="CAGxHudhlEBy060KTujVQktFYKbZfkMS"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <GithubProvider>
+      <App />
+    </GithubProvider>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
