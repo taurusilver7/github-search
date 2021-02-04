7843c6c3c766bfffa8eb0a4ba3e7d1a8936ef8bb
taurusilver7 <prasad20697@gmail.com>
2021-02-04 10:12:25 +0530

    mock user details for development

---
+++ src/context/MockData/MockFollower.js
@@ -0,0 +1,71 @@
+const MockFollower = [
+  {
+    login: "fahadashiq12",
+    id: 44393684,
+    node_id: "MDQ6VXNlcjQ0MzkzNjg0",
+    avatar_url: "https://avatars.githubusercontent.com/u/44393684?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/fahadashiq12",
+    html_url: "https://github.com/fahadashiq12",
+    followers_url: "https://api.github.com/users/fahadashiq12/followers",
+    following_url:
+      "https://api.github.com/users/fahadashiq12/following{/other_user}",
+    gists_url: "https://api.github.com/users/fahadashiq12/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/fahadashiq12/starred{/owner}{/repo}",
+    subscriptions_url:
+      "https://api.github.com/users/fahadashiq12/subscriptions",
+    organizations_url: "https://api.github.com/users/fahadashiq12/orgs",
+    repos_url: "https://api.github.com/users/fahadashiq12/repos",
+    events_url: "https://api.github.com/users/fahadashiq12/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/fahadashiq12/received_events",
+    type: "User",
+    site_admin: false,
+  },
+  {
+    login: "Soldy",
+    id: 4786022,
+    node_id: "MDQ6VXNlcjQ3ODYwMjI=",
+    avatar_url: "https://avatars.githubusercontent.com/u/4786022?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/Soldy",
+    html_url: "https://github.com/Soldy",
+    followers_url: "https://api.github.com/users/Soldy/followers",
+    following_url: "https://api.github.com/users/Soldy/following{/other_user}",
+    gists_url: "https://api.github.com/users/Soldy/gists{/gist_id}",
+    starred_url: "https://api.github.com/users/Soldy/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/Soldy/subscriptions",
+    organizations_url: "https://api.github.com/users/Soldy/orgs",
+    repos_url: "https://api.github.com/users/Soldy/repos",
+    events_url: "https://api.github.com/users/Soldy/events{/privacy}",
+    received_events_url: "https://api.github.com/users/Soldy/received_events",
+    type: "User",
+    site_admin: false,
+  },
+  {
+    login: "salick-77",
+    id: 61610355,
+    node_id: "MDQ6VXNlcjYxNjEwMzU1",
+    avatar_url: "https://avatars.githubusercontent.com/u/61610355?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/salick-77",
+    html_url: "https://github.com/salick-77",
+    followers_url: "https://api.github.com/users/salick-77/followers",
+    following_url:
+      "https://api.github.com/users/salick-77/following{/other_user}",
+    gists_url: "https://api.github.com/users/salick-77/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/salick-77/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/salick-77/subscriptions",
+    organizations_url: "https://api.github.com/users/salick-77/orgs",
+    repos_url: "https://api.github.com/users/salick-77/repos",
+    events_url: "https://api.github.com/users/salick-77/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/salick-77/received_events",
+    type: "User",
+    site_admin: false,
+  },
+];
+
+export default MockFollower;
