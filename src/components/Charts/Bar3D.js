8d5b9c2d629b6b7a9996e7d61533c66a284792ca
taurusilver7 <prasad20697@gmail.com>
2021-02-04 10:05:27 +0530

    bar & pie charts added

---
+++ src/components/Charts/Bar3D.js
@@ -0,0 +1,84 @@
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+// STEP 2 - Chart Data
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "bar3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: "Most Forked",
+        //Set the chart subcaption
+        // subCaption: "In MMbbl = One Million barrels",
+        //Set the x-axis name
+        xAxisName: "Repos",
+        // set x-axis font size
+        xAxisNameFontSize: "16px",
+        //Set the y-axis name
+        yAxisName: "Forks",
+        // set y-axis font size
+        yAxisNameFontSize: "16px",
+        // numberSuffix: "K",
+        //Set the theme for your chart
+        // theme: "fusion",
+        // set the decimal range
+        decimal: 0,
+        // set the pie-radius value
+        // pieRadius: "35%",
+        showCanvasBorder: 0,
+        showAlternateVGridColor: 0,
+        usePlotGradientColor: 0,
+        valueFontSize: 16,
+        placeValuesInside: 0,
+        divLineColor: "#102a42",
+        divLineAlpha: 15,
+        captionFontColor: "#102a42",
+        captionFontBold: 0,
+        captionFontSize: 20,
+        captionFont: "Roboto",
+        baseFont: "Open Sans",
+        baseFontSize: 12,
+        baseFontColor: "#617d98",
+        smartLineColor: "#617d98",
+        showShadow: 0,
+        showPlotBorder: 0,
+        // set pallate colors in bar
+        paletteColors:
+          "#2caeba, #5D62B5, #FFC533, #F2726F, #8d6e63, #1de9b6, #6E80CA",
+        bgColor: "#FFFFFF",
+        showBorder: 0,
+      },
+      // Chart Data
+      // data: data, <=> data,
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default ChartComponent;
